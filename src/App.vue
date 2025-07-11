44cee56e9ef4aec2e8780a0b7f868bf6344a6f44
Diego Reyes <diegorele13@gmail.com>
2025-07-11 11:28:41 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Footer from './components/globals/Footer.vue';
+import Header from './components/globals/Header.vue';
+
+</script>
+
+<template>
+  <Header/>
+  <main>
+    <RouterView />
+  </main>
+  <Footer />
+</template>
+
+
+<style lang="scss">
+@use '@/styles/index.scss' as *;
+
+.main-content {
+  min-height: 100vh;
+}
+
+.main-content.with-header {
+  margin-top: 56px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>
+    esta es el home
+  </p>
+</template>
